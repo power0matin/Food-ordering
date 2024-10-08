8d89d995f0d0e76f9833da6ae606ed63b42dc968
Mehrbod <mehrbodmehrabani@gmail.com>
2024-10-08 18:21:16 +0330

    all table updated

---
+++ model/service/table_service.py
@@ -0,0 +1,31 @@
+from model.entity.table import Table
+from model.repository.crud_repository import CrudRepository
+class TableService:
+    repo = CrudRepository(Table)
+
+    @classmethod
+    def save(cls, table):
+        cls.repo.save(table)
+        return table
+
+    @classmethod
+    def edit(cls, table):
+        cls.repo.edit(table)
+        return table
+
+    @classmethod
+    def remove(cls, id):
+        cls.repo.remove(id)
+        return id
+
+    @classmethod
+    def find_all(cls):
+        return cls.repo.find_all()
+
+    @classmethod
+    def find_empty_table(cls):
+        return cls.repo.find_by(Table.is_empty == is_empty)
+
+    @classmethod
+    def find_by_number(cls, number):
+        return cls.repo.find_by(Table.number == number)
