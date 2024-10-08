92c42306a211e0bf64af725b3378a1aabcbad1a2
minww1 <mina.rezaee001@gmail.com>
2024-10-08 17:36:29 -0500

    added drink

---
+++ model/service/drink_service.py
@@ -0,0 +1,28 @@
+class drink:
+    repo = CrudRepository(drink)
+
+    @classmethod
+    def save(cls, title, price, duration, status=True):
+        new_drink = Drink(title=title, price=price, duration=duration, status=status)
+        return cls.repo.save(new_drink)
+
+    @classmethod
+    def edit(cls, drink):
+        return cls.repo.edit(drink)
+
+    @classmethod
+    def remove(cls, drink_id):
+        cls.repo.remove(drink_id)
+        return drink_id
+
+    @classmethod
+    def find_by_id(cls, drink_id):
+        return cls.repo.find_by_id(drink_id)
+
+    @classmethod
+    def find_all(cls):
+        return cls.repo.find_all()
+
+    @classmethod
+    def find_by_title(cls, title):
+        return cls.repo.find_by(drink._title.like(f"%{title}%"))
