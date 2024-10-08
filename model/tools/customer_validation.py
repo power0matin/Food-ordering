f233db8cfdd752836c7a371109839d209f4eb3ec
AidaShams <aidashmas77@gmail.com>
2024-10-08 16:07:18 +0330

    customer_validation.py update

--- model/tools/customer_validation.py
+++ model/tools/customer_validation.py
@@ -5,7 +5,7 @@ class CustomerValidation:
 
     @staticmethod
     def id_validator(customer_id, message):
-        if type(customer_id) == int and re.match(r"^[1-9]{9}$", customer_id):
+        if type(customer_id) == int and re.match(r"^[1-9][0-9]{0,6}$", customer_id):
             return customer_id
         else:
             return ValueError(message)
@@ -18,15 +18,22 @@ class CustomerValidation:
             raise ValueError(message)
 
     @staticmethod
-    def email_validator(email, message):
-        if re.match(r"^[a-zA-Z0-9]\w+@(gmail|yahoo).com$", email, re.I):
-            return email
+    def family_validator(family, message):
+        if type(family) == str and re.match(r"^[a-zA-Z\s]{2,20}$", family):
+            return family
         else:
             raise ValueError(message)
 
     @staticmethod
-    def phone_validator(phone, message):
-        if type(phone) == str and re.match(r"^09[0-9]{9}$", phone):
-            return phone
+    def username_validator(username, message):
+        if type(username) == str and re.match(r"^[a-zA-Z0-9\s]{2,20}$", username):
+            return username
+        else:
+            raise ValueError(message)
+
+    @staticmethod
+    def password_validator(password, message):
+        if type(password) == str and re.match(r"^[a-zA-Z0-9]{8,}$", password):
+            return password
         else:
             raise ValueError(message)
