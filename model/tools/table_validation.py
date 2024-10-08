c7bf125711e7854d8ffb340739b7ec741b176540
Mehrbod <mehrbodmehrabani@gmail.com>
2024-10-08 17:14:41 +0330

    table validation

--- model/tools/table_validation.py
+++ model/tools/table_validation.py
@@ -4,10 +4,10 @@ class TableValidation:
 
     @staticmethod
     def id_validator(id,message):
-        if type(id) == int and re.match(r"^[1-9]{7}$", id):
+        if type(id) == int and id >= 0:
             return id
         else:
-            return ValueError(message)
+            raise ValueError(message)
 
 
     @staticmethod
@@ -15,28 +15,28 @@ class TableValidation:
         if type(title) == str and re.match(r"^[A-Za-z\s]$", title):
             return title
         else:
-            return ValueError(message)
+            raise ValueError(message)
 
     @staticmethod
     def location_validator(location,message):
         if type(location) == str and re.match(r"^[A-Za-z\s0-9]$", location):
             return location
         else:
-            return ValueError(message)
+            raise ValueError(message)
 
     @staticmethod
     def number_validator(number,message):
         if type(number) == int and re.match(r"^[1-9]{2}$", number):
             return number
         else:
-            return ValueError(message)
+            raise ValueError(message)
 
     @staticmethod
     def is_empty_validator(is_empty,message):
         if type(is_empty) == bool:
             return is_empty
         else:
-            return ValueError(message)
+            raise ValueError(message)
 
 
 
