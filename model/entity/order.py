97a152b8b29cf09f0c72ebd1463c10b3068bba28
Shayan Emami <amirshayanemami@gmail.com>
2024-10-08 17:25:08 +0330

    order.py updated

--- model/entity/order.py
+++ model/entity/order.py
@@ -1,5 +1,9 @@
+from sqlalchemy.orm import relationship
+
 from model.entity.base import Base
-from sqlalchemy import Column, Integer, DateTime, String
+from sqlalchemy import Column, Integer, DateTime, String, ForeignKey
+from model.entity.customer import Customer
+from model.entity.food import Food
 from model.tools.validation import Validation
 from model.tools.order_validation import OrderValidation
 
@@ -8,10 +12,26 @@ class Order(Base):
     __tablename__ = "order_table"
 
     _id = Column("id", Integer, primary_key=True, autoincrement=True)
+
     _amount = Column("amount", Integer, nullable=False)
+
     _discount = Column("discount", Integer, default=0)
+
     _pure_amount = Column("pure_amount", Integer, default=_amount)
+
     _date_time = Column("date_time", DateTime)
+
+    _customer_name = Column("customer_name", String(20), ForeignKey("customer_tbl.name"), nullable=False)
+    customer_name = relationship("customer", back_populates="order_table")
+
+    _food_title = Column("food", String(20), ForeignKey("food_tbl.title"))
+    food = relationship("food", back_populates="order_table")
+
+    _drink = Column("drink", String(20), ForeignKey("drink_tbl.title"))
+    drink = relationship("drink", back_populates="order_table")
+
+    _table = Column("table", String(20), ForeignKey("table_tbl.title"))
+    table = relationship("table", back_populates="order_table")
 
     def __init__(self, order_id, table, customer, amount, discount, pure_amount):
         self.order_id = order_id
