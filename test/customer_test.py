0738de0dd60e1f94f5707605a881d5bf201e038b
AidaShams <aidashmas77@gmail.com>
2024-10-10 00:15:13 +0330

    test customer done and well yay! by aida

--- test/customer_test.py
+++ test/customer_test.py
@@ -2,6 +2,6 @@
 from model.entity.customer import Customer
 
 
-customer = Customer(123, "aida", "sams", "aidashmas77mail.com", "091aa456789", "aida-123", "aidas-123")
-# validation works okay but shows None instead of message!!!(email, phone)
+customer = Customer(123, "ai0da", "sH9ams", "aidashmas77mail.com", "091aa456789", "aida-123", "aidas-123")
+
 print(customer)
