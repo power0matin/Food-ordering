d606076a5a838ae4368c6566cc2833fff030d3f6
Matin Shahabadi <116592301+power0matin@users.noreply.github.com>
2024-10-13 14:31:26 +0330

    improve admin_controller.py

---
+++ controller/admin_controller.py
@@ -0,0 +1,104 @@
+from model.entity.admin import Admin
+from model.service.admin_service import AdminService
+
+
+class AdminController:
+
+    @classmethod
+    def save(cls, name, family, username, password, access_level):
+        try:
+            admin = Admin(name, family, username, password, access_level)
+            AdminService.save(admin)
+            return True, "Admin Saved."
+        except Exception as e:
+            return False, str(e)
+
+    @classmethod
+    def edit(cls, name, family, username, password, access_level):
+        try:
+            admin = Admin(name, family, username, password, access_level)
+            AdminService.edit(admin)
+            return True, "Admin Edited."
+        except Exception as e:
+            return False, str(e)
+
+    @classmethod
+    def remove(cls, id):
+        try:
+            AdminService.remove(id)
+            return True, "Admin Removed."
+        except Exception as e:
+            return False, str(e)
+
+    @classmethod
+    def find_all(cls):
+        try:
+            return True, AdminService.find_all()
+        except Exception as e:
+            return False, str(e)
+
+    @classmethod
+    def find_by_id(cls, id):
+        try:
+            return True, AdminService.find_by_id(id)
+        except Exception as e:
+            return False, str(e)
+
+    from model.entity.admin import Admin
+    from model.service.admin_service import AdminService
+
+    class AdminController:
+
+        @classmethod
+        def save(cls, name, family, username, password, access_level):
+            try:
+                admin = Admin(None, name, family, username, password, access_level)
+                AdminService.save(admin)
+                return True, "Info: Admin Saved!"
+            except Exception as e:
+                return False, str(e)
+
+        @classmethod
+        def edit(cls, id, name, family, username, password, access_level):
+            try:
+                admin = Admin(id, name, family, username, password, access_level)
+                AdminService.edit(admin)
+                return True, "Admin Has Been Edited!"
+            except Exception as e:
+                return False, str(e)
+
+        @classmethod
+        def remove(cls, id):
+            try:
+                AdminService.remove(id)
+                return True, "Info: Admin Removed!"
+            except Exception as e:
+                return False, str(e)
+
+        @classmethod
+        def find_all(cls):
+            try:
+                return True, AdminService.find_all()
+            except Exception as e:
+                return False, str(e)
+
+        @classmethod
+        def find_by_username(cls, username):
+            try:
+                return True, AdminService.find_by_username(username)
+            except Exception as e:
+                return False, str(e)
+
+        @classmethod
+        def find_by_username_password(cls, username, password):
+            try:
+                return True, AdminService.find_by_username_password(username, password)
+            except Exception as e:
+                return False, str(e)
+
+        @classmethod
+        def find_by_access_level(cls, access_level):
+            try:
+                return True, AdminService.find_by_access_level(access_level)
+            except Exception as e:
+                return False, str(e)
