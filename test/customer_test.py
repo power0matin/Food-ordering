c535e39704128c4a0b6458d07d73ba7ed16ffe64
AidaShams <aidashmas77@gmail.com>
2024-10-13 17:10:53 +0330

    costumer test

--- test/customer_test.py
+++ test/customer_test.py
@@ -1,7 +1,13 @@
 #customer test - Aida Shams
 from model.entity.customer import Customer
-
+from controller.customer_controller import CustomerController
 
 customer = Customer(123, "ai0da", "sH9ams", "aidashmas77mail.com", "091aa456789", "aida-123", "aidas-123")
 
 print(customer)
+
+
+
+CustomerController.save("ali", "alipour","aidashmas77@gmail.com","09111234567", "aida123", "aidash123")
+CustomerController.find_all()
+CustomerController.find_by_id(1)
