1e744764fe1a31e2c287f642395acc0a131fa5a1
AidaShams <aidashmas77@gmail.com>
2024-10-15 14:36:28 +0330

    view test --Aida

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
 from controller.admin_controller import AdminController
+from controller.customer_controller import CustomerController
 from model.entity.base import Base
+from view.customer_view import CustomerView
 
 connection_string = "mysql+pymysql://root:root123@localhost:3306/mft"
 if not database_exists(connection_string):
@@ -16,3 +18,6 @@ AdminController.save("matin", "shahabadi", "powermatin", "abcd1234", "1")
 
 u = AdminController.find_by_id(1)
 print(u)
+
+CustomerController.save("aida", "shams", "asHe77@gmail.com", "09114567893", "aidaaa", "aida1234")
+CustomerView()
