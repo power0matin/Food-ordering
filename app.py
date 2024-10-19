3c4fd58bf317c2c5cf8f054432e46fa0438a103e
AidaShams <aidashmas77@gmail.com>
2024-10-19 22:12:24 +0330

    app test hashtags added-> aida

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
 from controller.admin_controller import AdminController
 from controller.customer_controller import CustomerController
+from controller.payment_controller import PaymentController
 from model.entity import Base
 from view.customer_view import CustomerView
 from view.payment_view import PaymentView
@@ -20,7 +21,10 @@ AdminController.save("matin", "shahabadi", "powermatin", "abcd1234", "1")
 u = AdminController.find_by_id(1)
 print(u)
 
+#costumer app test
 CustomerController.save("aida", "shams", "asHe77@gmail.com", "09114567893", "aidaaa", "aida1234")
 CustomerView()
 
+#payment app test
+PaymentController.save(3, "online", "blah blah", "pizza with grilled onions and meat toppings")
 PaymentView()
