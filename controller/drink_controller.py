b1841e36fd61c29dae8fcbc96dd20f4e42fdd91a
minww1 <mina.rezaee001@gmail.com>
2024-10-20 16:18:35 -0500

    new

--- controller/drink_controller.py
+++ controller/drink_controller.py
@@ -5,42 +5,42 @@ from model.tools.decorators import exception_handling
 
 class DrinkController:
 
+
     @classmethod
     @exception_handling
-    def add_drink(cls, title, price, duration, size, status=True):
-        drink = Drink(None, title, price, duration, size, status)
+    def save(cls, title, price, duration, size, available=True):
+        drink = Drink(None, title, price, duration, size, available)
         DrinkService.save(drink)
-        return "Drink Saved"
+        return True, "Drink Saved!"
 
     @classmethod
     @exception_handling
-    def update_drink(cls, drink_id, title=None, price=None, duration=None, size=None, status=None):
-        drink = Drink(drink_id, title, price, duration, size, status)
+    def edit(cls, id, title, price, duration, size, available=True):
+        drink = Drink(id, title, price, duration, size, available)
         DrinkService.edit(drink)
-        return "Drink Updated"
+        return True, "Drink Edited!"
+
 
     @classmethod
     @exception_handling
-    def delete_drink(cls, drink_id):
-        DrinkService.remove(drink_id)
-        return "Drink Deleted"
+    def remove(cls, id):
+        DrinkService.remove(id)
+        return True, "Drink Removed!"
+
 
     @classmethod
     @exception_handling
-    def get_drink_by_id(cls, drink_id):
-        return True, DrinkService.find_by_id(drink_id)
-
-    @classmethod
-    @exception_handling
-    def get_all_drinks(cls):
+    def find_all(cls):
         return True, DrinkService.find_all()
 
-    @classmethod
-    @exception_handling
-    def search_drink_by_title(cls, title):
-        return True, DrinkService.find_by_title(title)
 
     @classmethod
     @exception_handling
-    def search_drink_by_title_and_status(cls, title, status):
-        return True, DrinkService.search_drink_by_title_and_status(title, status)
+    def find_by_id(cls, id):
+        return True, DrinkService.find_by_id(id)
+
+
+    @classmethod
+    @exception_handling
+    def find_by_title(cls, title):
+        return True, DrinkService.find_by_title(title)
