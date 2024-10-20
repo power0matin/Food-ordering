9bb2fd43254c0bc930020a58e6c6ab5740c50ebb
Shayan Emami <amirshayanemami@gmail.com>
2024-10-20 15:12:54 +0330

    order.py updated

--- test/order_test.py
+++ test/order_test.py
@@ -5,3 +5,8 @@ from model.entity.order import *
 # todo : test is not complete
 # OrderController.save(1, 42, 0, 213)
 
+OrderController.save(123,"40%", 123)
+OrderController.edit(125,123, "30%", 123)
+OrderController.remove(1)
+OrderController.find_all()
+OrderController.find_by_id(1)
