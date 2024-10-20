1a30365708cb9c0f55ce27a7fdb3274d4423d193
AidaShams <aidashmas77@gmail.com>
2024-10-20 15:56:14 +0330

    payment_view.py edited -> aida

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
 from controller.admin_controller import AdminController
+from controller.customer_controller import CustomerController
 from controller.payment_controller import PaymentController
 from model.entity import Base
+from view.customer_view import CustomerView
 from view.payment_view import PaymentView
 
 connection_string = "mysql+pymysql://root:root123@localhost:3306/mft"
@@ -20,11 +22,11 @@ u = AdminController.find_by_id(1)
 print(u)
 #
 # costumer app test
-# CustomerController.save("aida", "shams", "asHe77@gmail.com", "09114567893", "aidaaa", "aida1234")
-# CustomerView()
+CustomerController.save("aida", "shams", "asHe77@gmail.com", "09114567893", "aidaaa", "aida1234")
+#CustomerView()
 #
 # payment app test
 # todo : has error
 # error fixed
 PaymentController.save(3, "online", "blah blah", 1)
-PaymentView()
+#PaymentView()
