2fb8c5f7c3c6970f1780beb44d999f85e9d29427
Mehrbod <mehrbodmehrabani@gmail.com>
2024-10-20 18:03:50 +0330

    table updated

--- test/table_test.py
+++ test/table_test.py
@@ -1,13 +1,13 @@
 from controller.table_controller import TableController
 
 # todo : test failes
-TableController.save(" adad", "", 12, True )
-# TableController.save("left", "mashad", "12", True)
-# TableController.edit(1, "", "", "", False)
+TableController.save("table1", "right", 12, True)
+#TableController.edit(1, "dsfsd", "vxcvc", 13, False)
 # TableController.remove(1)
-# TableController.find_all()
+TableController.find_all()
 # TableController.find_empty_table()
 #
 # TableController.find_by_number(2)
+print(entity_list)
 
 
