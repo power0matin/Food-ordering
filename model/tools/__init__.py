c9c69c32ca78232d4e95b9b5f0f0666844b3b884
power0matin <matinshahabadi@outlook.com>
2024-10-20 14:29:08 +0330

    Update project

--- model/tools/__init__.py
+++ model/tools/__init__.py
@@ -2,6 +2,5 @@ from model.tools.decorators import exception_handling
 from model.tools.logging import Logger
 
 from model.tools.validation import Validation
-from model.tools.food_validation import FoodValidation
 from model.tools.order_validation import OrderValidation
 from model.tools.table_validation import TableValidation
