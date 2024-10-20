6547ff65734bb63634dbc859f4996481a1f63d82
minww1 <mina.rezaee001@gmail.com>
2024-10-20 13:15:52 -0500

    new validation.py

--- model/tools/validation.py
+++ model/tools/validation.py
@@ -77,4 +77,38 @@ class Validation:
         else:
             return ValueError(message)
 
+    @staticmethod
+    def drink_validator(drink, message):
+        if type(drink) == str and re.match(r"^[a-zA-Z\s]{3,20}$", drink):
+            return drink
+        else:
+            raise ValueError(message)
+
+    @staticmethod
+    def title_validator(title, error_message):
+        if not title or len(title) > 50:
+                raise ValueError(error_message)
+        return title
+
+    @staticmethod
+    def price_validator(price, error_message):
+        if price <= 0:
+            raise ValueError(error_message)
+        return price
+
+    @staticmethod
+    def duration_validator(duration, error_message):
+        if duration <= 0:
+            raise ValueError(error_message)
+        return duration
+
+    @staticmethod
+    def size_validator(size, error_message):
+        if not size or len(size) > 10:
+            raise ValueError(error_message)
+        return size
+
+
+
+
 
