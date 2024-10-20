521dd114933b364284b19c9222419f41f70a36d1
bardiazaghari <bardia.zaghari@yahoo.com>
2024-10-20 15:04:39 +0330

    updated

--- controller/food_controller.py
+++ controller/food_controller.py
@@ -5,37 +5,43 @@ from model.tools.decorators import exception_handling
 
 class FoodController:
 
+    # save:
     @classmethod
     @exception_handling
-    def add_food(cls, title, price, duration, description, size, status=True):
-        food = Food(None, title, price, duration, description, size, status)
+    def save(cls, title, description, price, duration, size, available=True):
+        food = Food(None, title, description, price, duration, size, available)
         FoodService.save(food)
-        return "Food Saved"
+        return True, "Food Saved!"
 
+    # edit:
     @classmethod
     @exception_handling
-    def update_food(cls, food_id, title=None, price=None, duration=None, description=None, size=None, status=None):
-        food = Food(food_id, title, price, duration, description, size, status)
+    def edit(cls, id, title, description, price, duration, size, available=True):
+        food = Food(id, title, description, price, duration, size, available)
         FoodService.edit(food)
-        return "Food Updated"
+        return True, "Food Edited!"
 
+    # remove:
     @classmethod
     @exception_handling
-    def delete_food(cls, food_id):
-        FoodService.remove(food_id)
-        return "Food Deleted"
+    def remove(cls, id):
+        FoodService.remove(id)
+        return True, "Food Removed!"
 
+    # find all:
     @classmethod
     @exception_handling
-    def get_food_by_id(cls, food_id):
-        return True, FoodService.find_by_id(food_id)
-
-    @classmethod
-    @exception_handling
-    def get_all_foods(cls):
+    def find_all(cls):
         return True, FoodService.find_all()
 
+    # find by id:
     @classmethod
     @exception_handling
-    def search_food_by_title(cls, title):
+    def find_by_id(cls, id):
+        return True, FoodService.find_by_id(id)
+
+    # find by title:
+    @classmethod
+    @exception_handling
+    def find_by_title(cls, title):
         return True, FoodService.find_by_title(title)
