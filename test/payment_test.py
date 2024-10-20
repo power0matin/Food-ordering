1fe32a58aea2e4e2066174fbab0c1b6fb296f19f
AidaShams <aidashmas77@gmail.com>
2024-10-20 16:51:08 +0330

    payment_test.py and customer_test.py edited -> aida

--- test/payment_test.py
+++ test/payment_test.py
@@ -2,12 +2,12 @@ from controller.payment_controller import PaymentController
 from model.entity import Payment
 
 # Save
-PaymentController.save(3, "online", "blah blah", 1)
+PaymentController.save(3, "online", "blah blah", "pizza")
 
 payment_id = 1
 
 # Edit
-PaymentController.edit(2,4, "in person", "description", 2)
+PaymentController.edit(2,4, "in person", "description", "fries")
 
 # Remove
 #PaymentController.remove(payment_id)
@@ -15,9 +15,22 @@ PaymentController.edit(2,4, "in person", "description", 2)
 # Find all
 all_payments = PaymentController.find_all()
 for payment in all_payments[1]:
-    print(f"ID: {payment.id}, Amount: {payment.amount}, Payment Type: {payment.payment_type}")
+    print(f"ID: {payment.id}, Amount: {payment.amount}, Payment Type: {payment.payment_type}, Order: {Payment.order}")
 
 #find by id
 payment = PaymentController.find_by_id(1)
 if payment:
-    print(f"Found Payment: Amount: {Payment.amount}, Payment Type: {Payment.payment_type}")
+    print(f"Found Payment: Amount: {Payment.amount}, Payment Type: {Payment.payment_type}, Order: {Payment.order}")
+
+#find by payment type
+payment = PaymentController.find_by_payment_type("online")
+if payment:
+    print(f"Found Payment: ID: {Payment.id}, Amount: {Payment.amount}, Payment Type: {Payment.payment_type}, Order: {Payment.order}")
+
+#find by amount
+payment = PaymentController.find_by_amount(1)
+if payment:
+    print(f"Found Payment: ID: {Payment.id}, Amount: {Payment.amount}, Payment Type: {Payment.payment_type}, Order: {Payment.order}")
+
+#find by order
+#payment = PaymentController.find_by_id(1)
