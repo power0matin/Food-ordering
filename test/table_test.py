6f375fb53441c2518d37f39534d551b5bd580e50
Mehrbod <mehrbodmehrabani@gmail.com>
2024-10-22 16:01:09 +0330

    table updated

--- test/table_test.py
+++ test/table_test.py
@@ -2,27 +2,25 @@ from controller.table_controller import TableController
 
 # todo : test failes
 
-# Save
-#TableController.save("table1", "right", 2, True)
+# Save +
+# TableController.save("table3", "right", 3, True)
 
-table_id = 1
+# Edit +
+# TableController.edit(14, "22", "left", 13, False)
 
-# Edit
-#TableController.edit(1, "22", "left", 13, False)
+# Remove +
+# TableController.remove(2)
 
-# Remove
-TableController.remove(table_id)
-
-# Find all
-table_findall = TableController.find_all()
-for table in table_findall[1]:
-   print(f"title: {table.title}, location: {table.location}, number: {table.number}, is_empty: {table.is_empty}")
+# Find all +
+# table_findall = TableController.find_all()
+# for table in table_findall[1]:
+#   print(f"title: {table.title}, location: {table.location}, number: {table.number}, is_empty: {table.is_empty}")
 
 # Find Empty Table
 
-# TableController.find_empty_table()
+TableController.find_empty_table()
 
-#Find By Number
+# Find By Number
 
 # TableController.find_by_number(2)
 
