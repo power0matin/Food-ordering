0a0d75838b74899ddd7e6261221d1b17939ae3d6
minww1 <mina.rezaee001@gmail.com>
2024-10-22 15:08:08 -0500

    new

--- test/drink_test.py
+++ test/drink_test.py
@@ -1,32 +1,33 @@
-from controller.drink_controller import DrinkController
-
 # todo : what is size? str ?
 # todo : what is size? str ?
 # todo : error
 # DrinkController.get_all_drinks()
 # todo error
-# DrinkController.search_drink_by_title("Pepsi")
-DrinkController.save("Pepsi", 3.0, 300, "integer", "Large")
+from controller.drink_controller import DrinkController
+
+
+DrinkController.save("Coca Cola", 1.5, 250, "Soft drink")
+
 drink_id = 1
 
-DrinkController.edit(drink_id, "Pepsi", 6.0, 9.99, "integer", "Medium")
-drink_id = 1
+
+DrinkController.edit(drink_id, "Pepsi", 1.2, 300, "Soft drink updated")
+
 
 DrinkController.remove(drink_id)
 
+
 all_drinks = DrinkController.find_all()
 for drink in all_drinks[1]:
-    print(f"ID: {drink}.id, Title: {drink}.title, Price: {drink}.price, Size: {drink}.size")
+    print(f"ID: {drink.id}, Title: {drink.title}, Price: {drink.price}, Size: {drink.size}, Duration: {drink.duration}")
 
 drink_id = 1
 drink = DrinkController.find_by_id(drink_id)[1]
 if drink:
-    print(f"Found Drink: Title: {drink.title}, Price: {drink.price}, Size: {drink.size}")
+    print(f"Found Drink: Title: {drink.title}, Price: {drink.price}, Size: {drink.size}, Duration: {drink.duration}")
 
-title = "Drink"
-status, drink_by_title = DrinkController.find_by_title(title)
-if status and drink_by_title:
-    for drink in drink_by_title:
-        print(f"ID: {drink.id}, Title: {drink.title}, Price: {drink.price}, Size: {drink.size}")
-
-
+title = "Coca Cola"
+status, drinks_by_title = DrinkController.find_by_title(title)
+if status and drinks_by_title:
+    for drink in drinks_by_title:
+        print(f"ID: {drink.id}, Title: {drink.title}, Price: {drink.price}, Size: {drink.size}, Duration: {drink.duration}")
