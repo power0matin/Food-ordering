013f962052eb4bb23fabfdb518d86c7bc51d4ef4
Matin Shahabadi <116592301+power0matin@users.noreply.github.com>
2024-10-26 23:24:02 +0330

    Update power0matin

--- model/repository/crud_repository.py
+++ model/repository/crud_repository.py
@@ -1,23 +1,13 @@
+# model/repository/crud_repository.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import create_database, database_exists, drop_database
-from model.entity import *
 
-connection_string = "mysql+pymysql://root:root123@localhost:3306/mft"
-if database_exists(connection_string):
-    drop_database(connection_string)
-create_database(connection_string)
 
-engine = create_engine(connection_string)
-
-try:
-    Base.metadata.drop_all(engine)
-except:
-    pass
-Base.metadata.create_all(engine)
+engine = create_engine("mysql+pymysql://root:Matin2170@localhost:3306/mft")
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
 
 class CrudRepository:
     def __init__(self, class_name):
@@ -27,29 +17,29 @@ class CrudRepository:
         session.add(entity)
         session.commit()
         session.refresh(entity)
-        return entity
+        return entity.to_tuple()
 
     def edit(self, entity):
         session.merge(entity)
         session.commit()
-        return entity
+        return entity.to_tuple()
 
     def remove(self, id):
         entity = session.get(self.class_name, id)
         if entity:
             session.delete(entity)
             session.commit()
-            return entity
+            return entity.to_tuple()
         return None
 
     def find_all(self):
         entity_list = session.query(self.class_name).all()
-        return entity_list
+        return [entity.to_tuple() for entity in entity_list]
 
     def find_by_id(self, id):
         entity = session.get(self.class_name, id)
-        return entity
+        return entity.to_tuple() if entity else None
 
     def find_by(self, find_statement):
-        entity = session.query(self.class_name).where(find_statement).all()
-        return entity
+        entity_list = session.query(self.class_name).filter(find_statement).all()
+        return [entity.to_tuple() for entity in entity_list]
